fc937479b32a071c6f8cdbbe4147de8430469446
Elke Heymans <el.heymans@gmail.com>
2025-12-11 22:47:45 +0100

    Caved for z3 solution for day 10

--- days/10.py
+++ days/10.py
@@ -1,4 +1,6 @@
+from typing import cast
 from utils.parse_file import parse_file
+from z3 import Int, Solver, Sum, sat, IntNumRef
 
 actual_input = parse_file("days/inputs/10.actual")
 example_input = parse_file("days/inputs/10.example")
@@ -35,7 +37,7 @@ def find_button_presses(machine: Machine) -> int:
     light_diagram = [False] * len(target_light_diagram)
     queue = []
     queue.append((0, light_diagram))
-    
+
     while len(queue) > 0:
         (presses, light_diagram) = queue.pop(0)
         if presses > 10:
@@ -60,6 +62,47 @@ def test_part_1_example():
     assert part_1(example_input) == 7
 
 
-# # takes 633 seconds...
-# def test_part_1_actual():
-#     assert part_1(actual_input) == 522
+# takes 633 seconds... beware
+def test_part_1_actual():
+    assert part_1(actual_input) == 522
+
+
+# tried it like part 1 but was way too slow
+# read that a lot of people tried things like A*, pruning, etc but still too slow or not guaranteed to find an answer
+# so looked into z3 for a solution as I was not going to implement a MILP solver myself
+def part_2(lines: list[str]) -> int:
+    machines = parse_input(lines)
+    total = 0
+    for _, button_wirings, joltage_requirements in machines:
+        solver = Solver()
+
+        button_vars = [Int(f"a{n}") for n in range(len(button_wirings))]
+        for var in button_vars:
+            solver.add(var >= 0)
+
+        for joltage_index, joltage_requirement in enumerate(joltage_requirements):
+            joltage_vars = [
+                button_vars[button_index]
+                for button_index, button in enumerate(button_wirings)
+                if joltage_index in button
+            ]
+            solver.add(Sum(joltage_vars) == joltage_requirement)
+
+        machine_result = 0
+        while solver.check() == sat:
+            model = solver.model()
+            machine_result = sum(
+                [cast(IntNumRef, model[result]).as_long() for result in model]
+            )
+            solver.add(Sum(button_vars) < machine_result)
+
+        total += machine_result
+    return total
+
+
+def test_part_2_example():
+    assert part_2(example_input) == 33
+
+
+def test_part_2_actual():
+    assert part_2(actual_input) == 18105
